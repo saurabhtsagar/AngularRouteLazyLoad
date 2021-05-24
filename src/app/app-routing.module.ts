d267883a74b7f4ee3d5cc1a43333346ae54de488
SAURABH SAGAR <mail.saurabhsagar@gmail.com>
2021-05-25 00:06:07 +0530

    Lazy modules added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'about', loadChildren: () => import('./modules/about/about.module').then(m => m.AboutModule) },
+  { path: 'setting', loadChildren: () => import('./modules/account/account.module').then(m => m.AccountModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
